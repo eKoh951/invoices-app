5028302f9298c14a70f4e0eb0fea9ced51b187f1
iAsure <asuregames01@gmail.com>
2023-02-22 17:31:25 -0600

    swagger test

--- apps/api/src/v1/users/dto/users.dto.ts
+++ apps/api/src/v1/users/dto/users.dto.ts
@@ -1,7 +1,13 @@
 import {} from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class UserDto {
+  @ApiProperty()
   id: string;
+
+  @ApiProperty()
   username: string;
+
+  @ApiProperty()
   email: string;
 }
--- apps/api/src/v1/users/users.controller.ts
+++ apps/api/src/v1/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Body, Param, Get } from "@nestjs/common";
 import { UsersServiceV1 } from "./users.service";
-import { UserDto } from "./dto/user.dto";
-import { ApiTags } from "@nestjs/swagger";
+import { UserDto } from "./dto/users.dto";
+import { ApiTags, ApiBody, ApiOperation, ApiParam } from "@nestjs/swagger";
+import { ApiOkResponse } from "@nestjs/swagger";
 
 @ApiTags("Users")
 @Controller({ path: "users", version: "1" })
@@ -9,7 +10,23 @@ export class UsersControllerV1 {
   constructor(private readonly usersService: UsersServiceV1) {}
 
   @Get()
-  getData() {
-    return "uwu";
+  @ApiBody({ type: [UserDto] })
+  @ApiOperation({ summary: 'Gets all available users' })
+  @ApiOkResponse({ description: "Successfully obtained users" })
+  getAllUsers(): UserDto[] {
+    return [{ id: "1", username: "adad", email: "dad" }];
+  }
+  @Get(':userId')
+  @ApiBody({ type: UserDto })
+  @ApiParam({
+    name:'userId',
+    type: 'string',
+    description: 'User unique id',
+    required: true
+  })
+  @ApiOperation({ summary: 'Gets the requested user' })
+  @ApiOkResponse({ description: "Successfully obtained user" })
+  getUser(@Param('userId') userId: string): UserDto {
+    return { id: "1", username: "adad", email: "dad" };
   }
 }
