848520a56fbb69ef64054b5471e4c5a7ec138a4c
iAsure <asuregames01@gmail.com>
2023-02-22 23:51:53 -0600

    swagger files fix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from "@nestjs/common";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { VersioningType } from "@nestjs/common/enums";
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setGlobalPrefix('api')
   app.enableVersioning({
@@ -24,8 +26,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("docs", app, document, {
     explorer: true,
-    url: 'test'
   });
+
+  app.useStaticAssets(join(__dirname, "../swagger"), { prefix: "docs" })
 
   await app.listen(process.env.API_PORT);
 }
