dbe6dc57baaa393762c84f2a36abd7ca9544a000
iAsure <asuregames01@gmail.com>
2023-02-23 23:16:47 -0600

    auth-config

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from "@nestjs/common";
+import { Module, NestModule, MiddlewareConsumer } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { UsersModuleV1 } from "./v1/users/users.module";
 import { InvoicesModuleV1 } from "./v1/invoices/invoices.module";
+
+import { ValidateAccessToken } from "./middlewares/auth.middleware";
 
 @Module({
   imports: [ConfigModule.forRoot(), UsersModuleV1, InvoicesModuleV1],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ValidateAccessToken).forRoutes("*");
+  }
+}
