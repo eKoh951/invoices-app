52aa597fbea4f0f1b2d6de87cb0c77c983194a37
iAsure <asuregames01@gmail.com>
2023-02-24 23:06:57 -0600

    branch format fix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
-import { VersioningType } from "@nestjs/common/enums";
-import { join } from 'path';
+
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe, VersioningType } from "@nestjs/common";
+
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
