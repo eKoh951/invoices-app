22ee26560b116c57ddfffe3206fdc3b0159484e3
iAsure <asuregames01@gmail.com>
2023-03-02 23:02:00 -0600

    env config destructuring

--- apps/api/src/config/env.config.ts
+++ apps/api/src/config/env.config.ts
@@ -1,24 +1,39 @@
 import { EnvConfig } from 'src/interfaces/env.config.interface';
 
-export default (): EnvConfig => ({
-  port: parseInt(process.env.API_PORT, 10) || 8000,
-  mongoUri: process.env.MONGO_URI,
-  auth0: {
-    app: {
-      domain: process.env.AUTH0_APP_DOMAIN,
-      audience: process.env.AUTH0_APP_AUDIENCE,
+export default function (): EnvConfig {
+  const {
+    API_PORT,
+    MONGO_URI,
+    AUTH0_APP_DOMAIN,
+    AUTH0_APP_AUDIENCE,
+    AUTH0_API_CLIENT_ID,
+    AUTH0_API_CLIENT_SECRET,
+    AUTH0_API_AUDIENCE,
+    AWS_ACCESS_KEY,
+    AWS_SECRET_KEY,
+    AWS_S3_BUCKET,
+  } = process.env;
+
+  return {
+    port: parseInt(API_PORT, 10) || 8000,
+    mongoUri: MONGO_URI,
+    auth0: {
+      app: {
+        domain: AUTH0_APP_DOMAIN,
+        audience: AUTH0_APP_AUDIENCE,
+      },
+      api: {
+        clientId: AUTH0_API_CLIENT_ID,
+        clientSecret: AUTH0_API_CLIENT_SECRET,
+        audience: AUTH0_API_AUDIENCE,
+      },
     },
-    api: {
-      clientId: process.env.AUTH0_API_CLIENT_ID,
-      clientSecret: process.env.AUTH0_API_CLIENT_SECRET,
-      audience: process.env.AUTH0_API_AUDIENCE,
+    aws: {
+      client: {
+        accessKey: AWS_ACCESS_KEY,
+        secretKey: AWS_SECRET_KEY,
+      },
+      s3Bucket: AWS_S3_BUCKET,
     },
-  },
-  aws: {
-    client: {
-      accessKey: process.env.AWS_ACCESS_KEY,
-      secretKey: process.env.AWS_SECRET_KEY,
-    },
-    s3Bucket: process.env.AWS_S3_BUCKET,
-  },
-});
+  };
+}
