c36e4e5ce9f945c3a20145c7898cc394eb6497ca
eKoh951 <greatekoh@gmail.com>
2023-04-06 00:38:51 -0600

    Fixed theming issue

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,21 +1,22 @@
 import * as React from "react";
 import Head from "next/head";
+import { themes } from "ui/themes";
+import ResponsiveDrawer from "ui/NavBar";
 import { AppProps } from "next/app";
-import { ThemeProvider, useTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
-import { themes } from "../../../packages/ui/themes";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 import createEmotionCache from "../src/createEmotionCache";
-import { League_Spartan } from "@next/font/google";
-import Home from "./page";
+// import { League_Spartan } from "@next/font/google";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 // Next code was writtten to try to apply the Spartan font
-const Spartan = League_Spartan({
-  subsets: ["latin"],
-  weight: ["500", "700"],
-});
+
+// This is causing a bug, will comment it
+// const Spartan = League_Spartan({
+//   subsets: ["latin"],
+//   weight: ["500", "700"],
+// });
 
 export interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
@@ -23,17 +24,17 @@ export interface MyAppProps extends AppProps {
 
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-  const theme = useTheme();
+
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={themes[theme.palette.mode]}>
-        <CssBaseline />
+    <ThemeProvider theme={themes.dark}>
+      <CssBaseline />
+      <ResponsiveDrawer window={undefined} />
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
         <Component {...pageProps} />
-        <Home />
-      </ThemeProvider>
-    </CacheProvider>
+      </CacheProvider>
+    </ThemeProvider>
   );
 }
---
+++ packages/ui/themes/light.theme.ts
@@ -0,0 +1,116 @@
+"use client";
+import { PaletteColor, PaletteColorOptions, createTheme } from "@mui/material";
+// import SpartanB from "../fonts/Spartan-Bold-700.ttf";
+// import SpartanM from "../fonts/Spartan-Medium-500.ttf";
+
+declare module "@mui/material/styles" {
+  interface Theme {
+    palette: {
+      draft: PaletteColor;
+    };
+  }
+
+  interface PaletteOptions {
+    draft: PaletteColorOptions;
+  }
+}
+
+export const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+    background: {
+      default: "#F8F8FB",
+    },
+    error: {
+      main: "#EC5757",
+      light: "#FF9797",
+      contrastText: "white",
+    },
+    warning: {
+      main: "#FF8F00",
+      dark: "rgba(255, 143, 0, 0.0571)     ",
+    },
+    success: {
+      main: "#33D69F",
+      dark: "rgba(51, 214, 159, 0.0571)",
+    },
+    draft: {
+      main: "#DFE3FA",
+      contrastText: "rgba(223, 227, 250, 0.0571)",
+    },
+  },
+  typography: {
+    fontFamily: ["SpartanM", "SpartanB"].join(","),
+    h1: {
+      fontSize: "32px",
+      fontWeight: "700",
+      lineHeight: "36px",
+      letterSpacing: "-1px",
+    },
+    h2: {
+      fontSize: "20px",
+      fontWeight: "700",
+      lineHeight: "22px",
+      letterSpacing: "-0.63px",
+    },
+    h3: {
+      fontSize: "16px",
+      fontWeight: "700",
+      lineHeight: "24px",
+      letterSpacing: "-0.8px",
+    },
+    h4: {
+      fontSize: "12px",
+      fontWeight: "700",
+      lineHeight: "15px",
+      letterSpacing: "-0.25px",
+    },
+    body1: {
+      fontSize: "12px",
+      fontWeight: "500",
+      lineHeight: "15px",
+      letterSpacing: "-0.25px",
+    },
+    body2: {
+      fontSize: "11px",
+      fontWeight: "500",
+      lineHeight: "18px",
+      letterSpacing: "-0.23px",
+    },
+    subtitle1: {
+      fontSize: "11px",
+      fontWeight: "500",
+      lineHeight: "18px",
+      letterSpacing: "-0.23px",
+      color: "#7E88C3",
+    },
+    overline: {
+      fontSize: "120px",
+      fontWeight: "700",
+      lineHeight: "137px",
+      letterSpacing: "-3.75px",
+    },
+    h5: {
+      fontSize: "20px",
+      fontWeight: "700",
+      lineHeight: "39px",
+      letterSpacing: "8px",
+      color: "#888EB0",
+    },
+    button: {
+      fontSize: "12px",
+      fontWeight: "700",
+      lineHeight: "15px",
+      letterSpacing: "-0.25px",
+      textTransform: "none",
+    },
+    // Commenting this for now, it is causing a bug
+    // poster: {
+    //   color: "white",
+    // },
+    // fontFamily: "Spartan",
+    // fontWeightMedium: "500",
+    // fontWeightBold: "700",
+    // textTransform: "none",
+  },
+});
