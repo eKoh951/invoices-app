2a253e2c06e26b81ad0dac079e0d563da48bc8b5
Víctor Ponce <107383836+victor-ponce@users.noreply.github.com>
2023-04-23 23:56:07 -0600

    invoiceList api fetch, date, etc

--- apps/web/pages/api/getAccessToken.ts
+++ apps/web/pages/api/getAccessToken.ts
@@ -1,14 +1,17 @@
-import { withApiAuthRequired, getAccessToken } from '@auth0/nextjs-auth0';
+// pages/api/getAccessToken.ts
+import { NextApiRequest, NextApiResponse } from "next";
+import { getAccessToken } from "@auth0/nextjs-auth0";
 
-export default withApiAuthRequired(async function getToken(req, res) {
+export interface AccessTokenResult {
+  accessToken?: string;
+}
+
+export async function getToken(req: NextApiRequest, res: NextApiResponse): Promise<AccessTokenResult> {
   try {
     const { accessToken } = await getAccessToken(req, res);
-    res.status(200).json({ accessToken });
-  } catch (error: any) {
+    return { accessToken }; // Devuelve un objeto con la propiedad accessToken
+  } catch (error) {
     console.error(error);
-    res.status(error.status || 500).json({
-      code: error.code,
-      error: error.message
-    });
+    throw error; // Lanza el error para que pueda ser manejado por getServerSideProps
   }
-});
+}
--- apps/web/pages/invoicesList.tsx
+++ apps/web/pages/invoicesList.tsx
@@ -1,97 +1,79 @@
-"use client";
-import {
-  Box,
-  FormControl,
-  IconButton,
-  InputLabel,
-  MenuItem,
-  Select,
-  Stack,
-  Typography,
-} from "@mui/material";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import { Button } from "ui/Button";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import StatusSquare from "../../../packages/ui/StatusCard";
+import { Box, Container } from "@mui/material";
 import { InvoiceCard } from "../../../packages/ui/InvoiceCard";
 import { InvoiceCreation } from "../../../packages/ui/InvoiceCreation";
+import { getSession } from "@auth0/nextjs-auth0";
+import { GetServerSideProps } from "next";
+import { getToken, AccessTokenResult } from "../pages/api/getAccessToken"; // Importa la función aquí desde el nuevo archivo
+import { Key } from "react";
 
-// Resultado similar a lo que daria la API al consultar un Invoice
-const invoiceExample = [
-  {
-    invoiceId: "INV-00004",
-    clientName: "Erick Huie",
-    ownerEmail: "eh@example.com",
-    status: "paid",
-    description: "Final Feliz",
-    paymentTerms: 30,
-    createdAt: "20 Apr 2023",
-    updatedAt: "21 Sep 2023",
-    totalAmount: "3500",
-  },
-  {
-    invoiceId: "INV-00003",
-    clientName: "Marco Sotelo",
-    ownerEmail: "Ms@example.com",
-    status: "paid",
-    description: "Cariniosas web",
-    paymentTerms: 30,
-    createdAt: "13 Apr 2023",
-    updatedAt: "15 Sep 2023",
-    totalAmount: "1700",
-  },
-  {
-    invoiceId: "INV-00002",
-    clientName: "John Smith",
-    ownerEmail: "mio@example.com",
-    status: "paid",
-    description: "Chat app",
-    paymentTerms: 30,
-    createdAt: "22 Apr 2023",
-    updatedAt: "23 Sep 2023",
-    totalAmount: "2500",
-  },
-  {
-    invoiceId: "INV-00001",
-    clientName: "John Doe",
-    ownerEmail: "owner@example.com",
-    status: "pending",
-    description: "Web design and hosting services",
-    paymentTerms: 30,
-    createdAt: "21 Aug 2021",
-    updatedAt: "20 Sep 2021",
-    totalAmount: "1500",
-  },
-];
+interface Invoice {
+  invoiceId: string;
+  createdAt: string;
+  clientName: string;
+  totalAmount: number;
+  status: string;
+}
+interface Props {
+  invoices: Invoice[];
+}
 
-export default function invoicesList() {
-  // const router = useRouter();
-  // const { data } = props;
-  // const navigatePage = () => router.push("/add-new");
-  const apiMethod = async () => {
-    const resToken = await fetch("api/getAccesToken");
-    const { accessToken } = await resToken.json();
-
-    const res = await fetch("http://localhost:8000/api/v1/invoices", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-  };
-
+export default function invoicesList({ invoices }: Props) {
   return (
     <Container maxWidth="desktop" sx={{ width: "730px" }}>
-      {/* ============= header ================== */}
+      {/* ============= header ==================  */}
       <InvoiceCreation />
-      {/* ======= invoice item =========== */}
+      {/* ======= invoice item ===========  */}
       <Box>
-        {invoiceExample.map((invoice) => (
+        {invoices.map((invoice: { invoiceId: Key | null | undefined }) => (
           <InvoiceCard key={invoice.invoiceId} invoice={invoice} />
         ))}
       </Box>
     </Container>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { req, res } = context;
+
+  // Comprueba si el usuario está autenticado
+  const session = await getSession(req, res);
+  if (!session || !session.user) {
+    return {
+      redirect: {
+        destination: "/login", // Redirige al usuario a la página de inicio de sesión si no está autenticado
+        permanent: false,
+      },
+    };
+  }
+
+  let accessTokenResult: AccessTokenResult;
+  try {
+    accessTokenResult = await getToken(req, res);
+  } catch (error) {
+    console.error("Error getting access token:", error);
+    return {
+      notFound: true,
+    };
+  }
+
+  const response = await fetch("http://localhost:8000/api/v1/invoices", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessTokenResult.accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    console.error("Error fetching invoices:", response.statusText);
+    return {
+      notFound: true,
+    };
+  }
+  const invoices = await response.json();
+
+  return {
+    props: {
+      invoices,
+    },
+  };
+};
